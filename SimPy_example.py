59dd12157603bfb775d063df31d5c012498bc6ad
Alicia White <awhite40@gatech.edu>
2016-04-01 09:50:55 -0400

    Creating basic airport example

--- SimPy_example.py
+++ SimPy_example.py
@@ -1,13 +1,35 @@
-def car(env):
-    while True:
-        print('Start parking at %d' % env.now)
-        parking_duration = 5
-        yield env.timeout(parking_duration)
-        print('Start driving at %d' % env.now)
-        trip_duration = 2
-        yield env.timeout(trip_duration)
 
 import simpy
+
+
+class Car(object):
+    def __init__(self, env):
+        self.env = env
+        self.action = env.process(self.run())
+
+    def run(self):
+        while True:
+            print('Start parking and charging at %d' % self.env.now)
+            charge_duration = 5
+            try:
+                yield self.env.process(self.charge(charge_duration))
+            except simpy.Interrupt:
+                print('Was interrupted.')
+
+            print('Start driving at %d' % env.now)
+            trip_duration = 2
+            yield self.env.timeout(trip_duration)
+
+    def charge(self, duration):
+        yield self.env.timeout(duration)
+
+
+def driver(env, car):
+    yield env.timeout(3)
+    car.action.interrupt()
+
+
 env = simpy.Environment()
-env.process(car(env))
+car = Car(env)
+env.process(driver(env,car))
 env.run(until=15)
