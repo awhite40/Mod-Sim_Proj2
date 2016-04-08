37be033e4e95ad1940007a49e3a42994ad933004
Alicia White <awhite40@gatech.edu>
2016-04-08 09:37:04 -0400

    Outline of abilities of central controller

---
+++ Central_controller.py
@@ -0,0 +1,8 @@
+
+# Recieve messages - process them in timestamp order
+
+# List of variables
+    # Airport gates
+    #Airplane queue
+# When service indicates that it is available send it message of where to go next
+
