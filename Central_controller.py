c610ad11e0775698edf461102428a5f29e1f7393
Alicia White <awhite40@gatech.edu>
2016-04-12 20:03:19 -0400

    update controller

--- Central_controller.py
+++ Central_controller.py
@@ -11,9 +11,10 @@ from mpi4py import MPI
 from MPI import ANY_SOURCE
 import simpy
 
-def Generate_airplane(env, ID, arrive, ):
+def Generate_airplane(env, ID, arrive, gate):
     Plane_ID = ID
-    if Gate_1_free == True:
+    yield env.timeout(arrive)
+    '''if Gate_1_free == True:
         Plane_Gate = 'Gate 1'
         Gate_1_free = False
         comm.Send(Plane_Gate, dest = 1)  # Schedule first service
@@ -31,14 +32,18 @@ def Generate_airplane(env, ID, arrive, ):
         comm.Send(Plane_Gate, dest=1)  # Schedule first service
     else:
         plane_queue = plane_queue + 1
-        #Add plane to queue
-    Departure_time = env.Now() + 30*60  # Now + 30 mins?
+        #Add plane to queue'''
+    with gate.request() as req:
+        yield req
+        # Plante_Gate =   determine gate number received ????
+        comm.Send(Plane_Gate, ID, dest=1)
+    
+    Departure_time = arrive + 30  # Now + 30 mins?
     return Plane_Gate, Departure_time, Plane_ID
 
 comm = MPI.COMM_WORLD
 rank = comm.Get_rank()
 plane_queue = 0
-
 if rank == 0:
     env = simpy.Environment()
     # Gate_1_free = True
@@ -47,7 +52,15 @@ if rank == 0:
     # Gate_4_free = True
     ID = 1
     gate = simpy.Resource(env, capacity = 4)
-    Gate, Departure_time, Plane_ID = Generate_airplane(ID)
+    for i = range(100):
+        ID = i
+        arrival_time = i*10
+        Gate, Departure_time, Plane_ID = env.process(Generate_airplane(env, ID, arrival_time, gate))
     comm.Recv(info, source=ANY_SOURCE)
+    env.run()
+
+if rank == 1:
+    # run code for process number 1
+
 
 
