dfcdad940745f36d9fe53793c3714459cd5d7134
jluo80 <jluo80@gatech.edu>
2016-04-14 20:33:30 -0400

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,37 +1,50 @@
 import random
+import sys
 import simpy
-from mpi4py import MPI
+
+from ServiceHub import ServiceHub as sh
+from Aircraft import Aircraft as ac
 
 # ================Setup================
 
 RANDOM_SEED = 42
-NUM_TRUCKS1 = 2     # Number of trucks for cleaning in the servicehub
-NUM_TRUCKS2 = 2     # Number of trucks for water supply in the servicehub
-NUM_TRUCKS3 = 2     # Number of trucks for power supply in the servicehub
-CLEANTIME = 10      # Minutes it takes to clean an aircraft
-WATERTIME = 10      # Minutes it takes to supply water to an aircraft
-POWERTIME = 10      # Minutes it takes to power up an aircraft
-SIM_TIME = 120      # Simulation time in minutes
+NUM_TRUCKS1 = 2             # Number of trucks for cleaning in the servicehub
+NUM_TRUCKS2 = 2             # Number of trucks for water supply in the servicehub
+NUM_TRUCKS3 = 2             # Number of trucks for power supply in the servicehub
+CLEANTIME = 10              # Minutes it takes to clean an aircraft
+WATERTIME = 10              # Minutes it takes to supply water to an aircraft
+POWERTIME = 10              # Minutes it takes to power up an aircraft
+SIM_TIME = 120              # Simulation time in minutes
+T_INTER = [30, 300]         # Create an aircraft every [min, max] mins
 
-class Main(env, num_trucks1, num_trucks2, num_trucks3, cleantime, watertime, powertime):
-    def __init__(self, env, name, size):
-        self.env = env
-        self.name = name
-        self.size = size
-    # Create the servicehub
-    servicehub = ServiceHub(env, num_trucks1, num_trucks2, num_trucks3, cleantime, watertime, powertime)
-    # Create one aircraft
-    aircraft = Aircraft(env, A1)
+# class Main(object):
+#     def __init__(self, env, num_trucks1, num_trucks2, num_trucks3, cleantime, watertime, powertime):
+#         self.env = env
+#         self.name = name
+#         self.size = size
+
+
+
+def aircraft_generator(env, service_hub, fuel_pump):
+    """Generate new aircrafts that arrive at the service hub."""
+    for i in itertools.count():
+        yield env.timeout(random.randint(*T_INTER))
+        env.process(aircraft('Aircraft %d' % i, env, service_hub, fuel_pump))
 
 # Setup and start the simulation
-print('Service Hub')
+print('Aircraft Service Ground Handling')
 random.seed(RANDOM_SEED)  # This helps reproducing the results
 
 # Create an environment and start the setup process
 env = simpy.Environment()
-env.process(setup(env, NUM_TRUCKS1, NUM_TRUCKS2, NUM_TRUCKS3, CLEANTIME, WATERTIME, POWERTIME))
+# Create the servicehub
+service_hub = sh(env, 2, 10)
+# Create one aircraft
+aircraft = ac(env, "A1", 1)
+env.process(gas_station_control(env, fuel_pump))
+env.process(aircraft_generator(env, gas_station, fuel_pump))
 
-# Execute!
-env.run(until=SIM_TIME)
+env.run(until=500)
 
-if __name__ == '__main__': main()
+
+# if __name__ == '__main__': Main()
