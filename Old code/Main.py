3c2c8785dcbe759fc9e8427c0e4a0a3ead4eea27
Alicia White <awhite40@gatech.edu>
2016-04-22 09:37:20 -0400

    Moving old folders

--- Old code/Main.py
+++ Old code/Main.py
@@ -22,7 +22,33 @@ T_INTER = [30, 300]         # Create an aircraft every [min, max] mins
 #         self.env = env
 #         self.name = name
 #         self.size = size
-
+def aircraft(name, env, service_hub, fuel_pump, Gates):
+    """A car arrives at the gas station for refueling.
+        
+        It requests one of the gas station's fuel pumps and tries to get the
+        desired amount of gas from it. If the stations reservoir is
+        depleted, the car has to wait for the tank truck to arrive.
+        
+        """
+    
+    
+    fuel_tank_level = random.randint(*FUEL_TANK_LEVEL)
+    print('%s arriving at airport at %.1f' % (name, env.now))
+    if 
+    with gas_station_control.request() as req:
+        start = env.now
+        # Request one of the gas pumps
+        yield req
+        
+        # Get the required amount of fuel
+        liters_required = FUEL_TANK_SIZE - fuel_tank_level
+        yield fuel_pump.get(liters_required)
+        
+        # The "actual" refueling process takes some time
+        yield env.timeout(liters_required / REFUELING_SPEED)
+        
+        print('%s finished refueling in %.1f seconds.' % (name,
+                                                          env.now - start))
 
 
 def aircraft_generator(env, service_hub, fuel_pump):
@@ -40,7 +66,6 @@ env = simpy.Environment()
 # Create the servicehub
 service_hub = sh(env, 2, 10)
 # Create one aircraft
-aircraft = ac(env, "A1", 1)
 env.process(gas_station_control(env, fuel_pump))
 env.process(aircraft_generator(env, gas_station, fuel_pump))
 
